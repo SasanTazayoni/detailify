7541032de3964f15bc27fc2b7cfd054278d57bba
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-11-14 22:42:43 +0000

    Add styles for hero header and text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,17 @@
 function Hero() {
   return (
-    <section className="p-4 grid grid-cols-1 md:grid-cols-2 gap-8 items-center mt-12">
+    <section className="p-6 grid grid-cols-1 md:grid-cols-2 gap-8 items-center mt-12">
       <div>
         <span className="badge inline-block text-white font-medium px-2 py-1 text-xs leading-4">
           New Release v2.0
         </span>
-        <h1 className="text-[60px] leading-[60px]">
-          Build with precision and <span>attention to detail</span>
+        <h1 className="text-[60px] leading-[60px] dark-text mt-1 mb-12">
+          Build with precision and{" "}
+          <span className="text-[48px] font-bold bg-linear-to-br from-[#155dfc] to-[#9810fa] bg-clip-text text-transparent">
+            attention to detail
+          </span>
         </h1>
-        <p>
+        <p className="text-[20px] leading-7 dark-grey-text">
           The modern platform for teams who care about quality. Every pixel
           matteres, every interaction counts.
         </p>
