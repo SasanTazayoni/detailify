52cb4816a350a0c6c31162b230b64f260203f5f3
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-11-15 00:23:41 +0000

    Add gradient status circles with overlapping layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,7 +15,7 @@ function Hero() {
           The modern platform for teams who care about quality. Every pixel
           matteres, every interaction counts.
         </p>
-        <div className="flex gap-4 mt-6 font-medium text-sm leading-5">
+        <div className="flex gap-4 mt-6 font-medium text-sm leading-5 mb-8">
           <button className="h-10 rounded-lg px-4 flex items-center gap-4 free-trial-btn text-white cursor-pointer">
             Start Free Trial
             <i className="fa-solid fa-arrow-right"></i>
@@ -24,12 +24,12 @@ function Hero() {
             Watch Demo
           </button>
         </div>
-        <div>
-          <div>
-            <div>circle</div>
-            <div>circle</div>
-            <div>circle</div>
-            <div>circle</div>
+        <div className="inline-block">
+          <div className="flex">
+            <div className="w-10 h-10 rounded-full border-2 border-white bg-[linear-gradient(to_bottom_right,#FB64B6,#FF6467)]"></div>
+            <div className="w-10 h-10 rounded-full border-2 border-white bg-[linear-gradient(to_bottom_right,#51A2FF,#00D3F2)] -ml-2 z-10"></div>
+            <div className="w-10 h-10 rounded-full border-2 border-white bg-[linear-gradient(to_bottom_right,#05DF72,#00D492)] -ml-2 z-20"></div>
+            <div className="w-10 h-10 rounded-full border-2 border-white bg-[linear-gradient(to_bottom_right,#C27AFF,#7C86FF)] -ml-2 z-30"></div>
           </div>
         </div>
         <div>
