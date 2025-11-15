abc4081cba41764a5c9d94690660938a45c0c4af
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-11-15 14:10:39 +0000

    Ad pricing component to main App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Features from "./components/Features";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
 import "./css/styles.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <div className="landing-page-and-dashboard">
         <Hero />
         <Features />
+        <Pricing />
       </div>
     </>
   );
