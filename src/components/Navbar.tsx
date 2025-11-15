da32b1732e2cf163353b5c52c8e824398207feb1
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-11-15 15:08:56 +0000

    Add appropriate letter spacing for elements on navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,19 +4,21 @@ function Navbar() {
       <div className="navbar-content flex justify-between items-center py-4 px-8">
         <div className="flex items-center gap-2">
           <div className="nav-logo"></div>
-          <span className="font-semibold dark-text">Detailify</span>
+          <span className="font-semibold dark-text tracking-[-0.31px]">
+            Detailify
+          </span>
         </div>
-        <div className="flex gap-8 dark-grey-text">
+        <div className="flex gap-8 dark-grey-text tracking-[-0.31px]">
           <a href="/">Features</a>
           <a href="/">Pricing</a>
           <a href="/">About</a>
           <a href="/">Dashboard</a>
         </div>
         <div className="flex gap-2 font-medium text-sm leading-5">
-          <button className="dark-text py-2 px-4 rounded-lg cursor-pointer">
+          <button className="dark-text py-2 px-4 rounded-lg cursor-pointer tracking-[-0.15px]">
             Sign In
           </button>
-          <button className="text-white py-2 px-4 rounded-lg register-btn cursor-pointer">
+          <button className="text-white py-2 px-4 rounded-lg register-btn cursor-pointer tracking-[-0.15px]">
             Get Started
           </button>
         </div>
