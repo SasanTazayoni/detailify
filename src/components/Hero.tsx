6cc76faa989ef190672eb88737eae017f333f066
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-11-15 00:51:50 +0000

    Add star rating display to hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -24,19 +24,32 @@ function Hero() {
             Watch Demo
           </button>
         </div>
-        <div className="inline-block">
-          <div className="flex">
-            <div className="w-10 h-10 rounded-full border-2 border-white bg-[linear-gradient(to_bottom_right,#FB64B6,#FF6467)]"></div>
-            <div className="w-10 h-10 rounded-full border-2 border-white bg-[linear-gradient(to_bottom_right,#51A2FF,#00D3F2)] -ml-2 z-10"></div>
-            <div className="w-10 h-10 rounded-full border-2 border-white bg-[linear-gradient(to_bottom_right,#05DF72,#00D492)] -ml-2 z-20"></div>
-            <div className="w-10 h-10 rounded-full border-2 border-white bg-[linear-gradient(to_bottom_right,#C27AFF,#7C86FF)] -ml-2 z-30"></div>
+        <div className="flex items-center gap-8">
+          <div className="inline-block">
+            <div className="flex">
+              <div className="w-10 h-10 rounded-full border-2 border-white bg-[linear-gradient(to_bottom_right,#FB64B6,#FF6467)]"></div>
+              <div className="w-10 h-10 rounded-full border-2 border-white bg-[linear-gradient(to_bottom_right,#51A2FF,#00D3F2)] -ml-2 z-10"></div>
+              <div className="w-10 h-10 rounded-full border-2 border-white bg-[linear-gradient(to_bottom_right,#05DF72,#00D492)] -ml-2 z-20"></div>
+              <div className="w-10 h-10 rounded-full border-2 border-white bg-[linear-gradient(to_bottom_right,#C27AFF,#7C86FF)] -ml-2 z-30"></div>
+            </div>
           </div>
-        </div>
-        <div>
-          <div>
-            <div>Trusted by 10,000+ developers</div>
+          <div className="inline-block">
+            <div>
+              <div className="text-sm dark-grey-text">
+                Trusted by 10,000+ developers
+              </div>
+            </div>
+            <div className="text-sm yellow-text flex items-center gap-2">
+              <div className="flex gap-0.5">
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+                <i className="fa-solid fa-star"></i>
+              </div>
+              <span>4.9/5 rating</span>
+            </div>
           </div>
-          <div>4.9/5 rating</div>
         </div>
       </div>
       <div>
