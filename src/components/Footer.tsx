6f1561df9da119ad7ebc9d09be8aa728f10081e4
Sasan Tazayoni <sasantazayoni@gmail.com>
2025-11-16 15:19:21 +0000

    Create footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="footer-text pt-[100px] pb-4 bg-[#101828] text-center text-sm dark-grey-text py-6 leading-5 tracking-[-0.15px]">
+      © 2025 Detailify. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
